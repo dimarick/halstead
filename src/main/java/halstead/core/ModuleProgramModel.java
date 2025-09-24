b8936338409d2f195ba081565d82be3bb4a308ea
dimarick <dimon.ksk@gmail.com>
2025-09-24 19:13:50 +0500

    #2 Структура модели метрик Холстеда

---
+++ src/main/java/halstead/core/ModuleProgramModel.java
@@ -0,0 +1,31 @@
+package halstead.core;
+
+public class ModuleProgramModel {
+    public double getCount(int countInputParams, int countOutputParams) {
+        return 0;
+    }
+
+    public double getProgramSize(double moduleCount) {
+        return 0;
+    }
+
+    public double getProgramVolume(double moduleCount) {
+        return 0;
+    }
+
+    public double getAssemblerInstructions(double moduleProgramSize) {
+        return 0;
+    }
+
+    public double getProgramProjectTime(double moduleCount, double programmerTeamSize, double programmerAvgPerformance) {
+        return 0;
+    }
+
+    public double getProgramBugCount(double moduleCount) {
+        return 0;
+    }
+
+    public double getProgramMeanTimeToFailure(double moduleCount) {
+        return 0;
+    }
+}
---
+++ src/main/java/halstead/core/ProgrammerModel.java
@@ -0,0 +1,13 @@
+package halstead.core;
+
+import halstead.dto.ProgramStat;
+
+public class ProgrammerModel {
+    public static double getRating(double initialRating, double abstractionLevel, ProgramStat[] programStats) {
+        return 0;
+    }
+
+    public static double getBugForecast(double rating, double abstractionLevel, double programVolume) {
+        return 0;
+    }
+}
